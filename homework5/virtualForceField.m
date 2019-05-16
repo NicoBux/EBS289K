f8d85a197a0153e20c714aa4ed4243e945918508
Guilherme <guilhermedemouraa@gmail.com>
2019-05-15 17:51:47 -0700

    Updated version

--- homework5/virtualForceField.m
+++ homework5/virtualForceField.m
@@ -43,8 +43,8 @@ for i=iMin2:iMax2
     for j=jMin2:jMax2
         if bitmap(i,j) > 0.8 %meaning there's an object
             [wX,wY] = IJtoXY(i,j,Xmax,Ymax,R,C); %coordinates in m of current position of the window
-            dx = abs(wX-x);
-            dy = abs(wY-y);
+            dx = wX-x;
+            dy = wY-y;
             d = sqrt(dx*dx+dy*dy); %distance from current window pixel to the robot in m
             Frx(i,j) = Fcr*bitmap(i,j)/(d*d)*(dx/d); %force component in xhat
             Fry(i,j) = Fcr*bitmap(i,j)/(d*d)*(dy/d); %force component in yhat
@@ -58,18 +58,13 @@ Fr(2) = sum(Fry(:)); %sum of force in yhat
 xT = T(1); yT = T(2);
 dxT = (xT-x); dyT = (yT-y);
 dT = sqrt(dxT*dxT+dyT*dyT);
-Fax = Fct*ones(iMax2-iMin2,jMax2-jMin2);
-Fay = Fax;
-Fax = Fax.*dxT/dT;
-Fay = Fay.*dyT/dT;
+Fax = Fct*dxT/dT; %sum of force in xhat
+Fay = Fct*dyT/dT; %sum of force in yhat
 
-Ft(1) = sum(Fax(:)); %sum of force in xhat
-Ft(2) = sum(Fay(:)); %sum of force in yhat
+FT(1) = Fax+Fr(1); %Resultant in xhat
+FT(2) = Fay+Fr(2); %Resultant in yhat
 
-FT(1) = Ft(1)-Fr(1); %Resultant in xhat
-FT(2) = Ft(2)-Fr(2); %Resultant in yhat
-
-w = 2; %force gain;
+%w = 2; %force gain;
 %FT(1) = w*FT(1)+(1-w)*FT(1)*-cos(theta); %fine tune of forces
 %FT(2) = w*FT(2)+(1-w)*FT(2)*-cos(theta); %fine tune of forces
 
@@ -77,8 +72,7 @@ mag = sqrt(FT(1)*FT(1)+FT(2)*FT(2)); %Force magnitude
 Fx = FT(1)/mag;
 Fy = FT(2)/mag;
 
-
-delta = atan(Fy/Fx);
+delta = atan2(Fy,Fx);
 Ks = 4;
 omega = Ks*angdiff(delta,theta);
 T = 0.1; %sampling rate;
